a95f94d4fa80b9e1f720433e769ed0449752b084
7diegojr <dfsntsjunior@gmail.com>
2025-08-18 11:36:51 -0300

    6° Commit - Finalizando os componentes e criando a tela de detalhes

--- pokedex/components/Card/index.jsx
+++ pokedex/components/Card/index.jsx
@@ -1,24 +1,33 @@
 import { Image, Text, View, ScrollView, TouchableOpacity } from "react-native";
-import {POKEMON_TYPE_COLORS} from '../../utils/colors';
+import { POKEMON_TYPE_COLORS } from '../../utils/colors';
 import { styles } from "./styles";
+import Button from "../Button";
+import { useRouter } from "expo-router";
 
-export default function Card ({ pokemon }) {
+export default function Card({ pokemon }) {
+    const router = useRouter();
     return (
         <TouchableOpacity
+            onPress={() =>
+                router.push({
+                    pathname: "/pokemon",
+                    params: { ...pokemon, Tipo: JSON.stringify(pokemon.Tipo) },
+                })
+            }
             style={[styles.container,
-                {backgroundColor: `${POKEMON_TYPE_COLORS[pokemon.Tipo[0].Nome]}`},
+            { backgroundColor: `${POKEMON_TYPE_COLORS[pokemon.Tipo[0].Nome]}` },
             ]}>
             <View style={styles.info}>
                 <Text style={styles.numero}>#{pokemon.Numero}</Text>
                 <Text style={styles.nome}>{pokemon.Nome}</Text>
                 <ScrollView horizontal>
                     {pokemon.Tipo.map((tipo) => (
-                        <Text>{tipo.Nome}</Text>
-                ))}
+                        <Button tipo={tipo} key={tipo.Nome} />
+                    ))}
                 </ScrollView>
             </View>
             <View style={styles.imageContainer}>
-                <Image source={{uri: pokemon.Imagem}} style={styles.imagem}/>
+                <Image source={{ uri: pokemon.Imagem }} style={styles.imagem} />
             </View>
         </TouchableOpacity>
     );
